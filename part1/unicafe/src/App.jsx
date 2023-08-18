8edb94f5dde174b3c8cfeacf68a34dd5e7771fcd
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2023-08-18 20:30:33 +0300

    1.8: unicafe step3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,12 +11,27 @@ const Button = ({ handleClick, text }) =>
 const Stat = ({ text, value }) => <p>{text} {value}</p>
 
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+
+  return (
+    <>
+      <Header content='statistics' />
+      <Stat text='good' value={good} />
+      <Stat text='neutral' value={neutral} />
+      <Stat text='bad' value={bad} />
+      <Stat text='all' value={all} />
+      <Stat text='average' value={(good - bad) / all} />
+      <Stat text='positive' value={(good / all * 100) + ' %'} />
+    </>
+  )
+}
+
+
 function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const all = good + neutral + bad
 
   return (
     <>
@@ -25,13 +40,7 @@ function App() {
       <Button handleClick={() => setNeutral(neutral => neutral + 1)} text='neutral' />
       <Button handleClick={() => setBad(bad => bad + 1)} text='bad' />
 
-      <Header content='statistics' />
-      <Stat text='good' value={good} />
-      <Stat text='neutral' value={neutral} />
-      <Stat text='bad' value={bad} />
-      <Stat text='all' value={all} />
-      <Stat text='average' value={(good - bad) / all} />
-      <Stat text='positive' value={(good / all * 100) + ' %'} />
+      <Statistics header='statistics' good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
