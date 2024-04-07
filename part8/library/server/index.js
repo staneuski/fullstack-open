e169c9b4e62db6ceb1d0d552735b6e1d157185a6
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2024-04-07 17:01:36 +0300

    8.7: Updating the birth year of an author

--- part8/library/server/index.js
+++ part8/library/server/index.js
@@ -116,6 +116,7 @@ const typeDefs = `
       published: Int!
       title: String!
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `
 
@@ -151,9 +152,19 @@ const resolvers = {
         })
       }
 
-      const book = { ...args, id: uuid() }
-      books = books.concat(book)
-      return book
+      const updatedBook = { ...args, id: uuid() }
+      books = books.concat(updatedBook)
+      return updatedBook
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((p) => p.name === args.name)
+      if (!author) return null
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map((author) =>
+        author.name === args.name ? updatedAuthor : author
+      )
+      return updatedAuthor
     }
   }
 }
