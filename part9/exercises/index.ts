5cda3a7091dfffa21b1d6d82af3763f4d2f4e054
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2024-07-08 17:16:23 +0300

    9.7 WebExercises

--- part9/exercises/index.ts
+++ part9/exercises/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import { calculateBmi } from './utils';
+import { calculateBmi, calculateExercises, convertArgs } from './utils';
 
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -10,16 +11,33 @@ app.get('/hello', (_req, res) => {
 app.get('/bmi', (req, res) => {
   const height: number = !isNaN(Number(req.query.height))
     ? Number(req.query.height)
-    : 0;
+    : NaN;
   const weight: number = !isNaN(Number(req.query.weight))
     ? Number(req.query.weight)
-    : 0;
+    : NaN;
   if (!weight || !height) {
     res.status(400).send({ error: 'malformatted parameters' });
   }
 
   const [_, description] = calculateBmi(height, weight);
   res.send({ height, weight, description });
+});
+
+app.post('/exercises', (req, res) => {
+  if (!('target' in req.body && 'daily_exercises' in req.body)) {
+    res.status(400).send({ error: 'parameters missing' });
+  }
+
+  try {
+    const [target, ...hours] = convertArgs(
+      6, // require minimum 5 days of exercise hours + target hours per day
+      [req.body.target, ...req.body.daily_exercises] as string[],
+      false,
+    );
+    res.send(calculateExercises(hours, target));
+  } catch (error: unknown) {
+    res.status(400).send({ error: 'malformatted parameters' });
+  }
 });
 
 const PORT = 3003;
