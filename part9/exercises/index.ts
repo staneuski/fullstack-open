32005e30a272c5932443a4957da4463b7e0bb63d
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2024-07-08 14:44:01 +0300

    9.6 Eslint

--- part9/exercises/index.ts
+++ part9/exercises/index.ts
@@ -1,9 +1,25 @@
 import express from 'express';
+import { calculateBmi } from './utils';
 
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const height: number = !isNaN(Number(req.query.height))
+    ? Number(req.query.height)
+    : 0;
+  const weight: number = !isNaN(Number(req.query.weight))
+    ? Number(req.query.weight)
+    : 0;
+  if (!weight || !height) {
+    res.status(400).send({ error: 'malformatted parameters' });
+  }
+
+  const [_, description] = calculateBmi(height, weight);
+  res.send({ height, weight, description });
 });
 
 const PORT = 3003;
