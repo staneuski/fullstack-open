8b1ec18bf5d657fff13dff94f579ee8d37ab0470
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2024-07-30 14:12:53 +0300

    9.11: Patientor backend, step4

--- part9/patientor/server/src/index.ts
+++ part9/patientor/server/src/index.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import diagnosesRouter from './routes/diagnoses';
+import patientsRouter from './routes/patients';
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,7 @@ app.use(cors()); // eslint-disable-line @typescript-eslint/no-unsafe-call
 const PORT = 3000;
 
 app.use('/api/diagnoses', diagnosesRouter);
+app.use('/api/patients', patientsRouter);
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
---
+++ part9/patientor/server/src/services/patientService.ts
@@ -0,0 +1,14 @@
+import { NonSensitivePatient } from '../types';
+import patients from '../../resources/patients';
+
+const getEntries = (): NonSensitivePatient[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+export default { getEntries };
