ae1e82ac9b9d71fd93a224ef4b53ef7a08ab90f6
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2024-08-06 18:55:37 +0300

    9.17: Safely add new diary

--- part9/flight-diary/src/components/DiaryForm.tsx
+++ part9/flight-diary/src/components/DiaryForm.tsx
@@ -1,8 +1,28 @@
-import { SyntheticEvent } from "react";
+import { useState, ChangeEventHandler, SyntheticEvent } from "react";
 
 import { useField } from "../hooks";
 
 import { NewDiary, Visibility, Weather } from "../types";
+
+interface VisibilityOption {
+  value: Visibility;
+  label: string;
+}
+const visibilityOptions: VisibilityOption[] = Object.values(Visibility).map(
+  (v) => ({
+    value: v,
+    label: v.toString(),
+  })
+);
+
+interface WeatherOption {
+  value: Weather;
+  label: string;
+}
+const weatherOptions: WeatherOption[] = Object.values(Weather).map((v) => ({
+  value: v,
+  label: v.toString(),
+}));
 
 interface Props {
   onSubmit: (values: NewDiary) => void;
@@ -10,22 +30,46 @@ interface Props {
 
 const DiaryForm = ({ onSubmit }: Props) => {
   const [date, resetDate] = useField("text", "date");
-  const [visibility, resetVisibility] = useField("text", "visibility");
-  const [weather, resetWeather] = useField("text", "title");
+  const [visibility, setVisibility] = useState(Visibility.Ok);
+  const [weather, setWeather] = useState(Weather.Cloudy);
   const [comment, resetComment] = useField("text", "comment");
+
+  const onVisibilityChange: ChangeEventHandler<HTMLSelectElement> = (event) => {
+    event.preventDefault();
+    if (typeof event.target.value === "string") {
+      const value = event.target.value;
+      const visibility = Object.values(Visibility).find(
+        (g) => g.toString() === value
+      );
+      if (visibility) {
+        setVisibility(visibility);
+      }
+    }
+  };
+
+  const onWeatherChange: ChangeEventHandler<HTMLSelectElement> = (event) => {
+    event.preventDefault();
+    if (typeof event.target.value === "string") {
+      const value = event.target.value;
+      const weather = Object.values(Weather).find(
+        (g) => g.toString() === value
+      );
+      if (weather) {
+        setWeather(weather);
+      }
+    }
+  };
 
   const submit = (event: SyntheticEvent) => {
     event.preventDefault();
     onSubmit({
       date: date.value,
-      visibility: visibility.value as Visibility,
-      weather: weather.value as Weather,
+      visibility,
+      weather,
       comment: comment.value,
     });
 
     void resetDate();
-    void resetVisibility();
-    void resetWeather();
     void resetComment();
   };
 
@@ -39,11 +83,23 @@ const DiaryForm = ({ onSubmit }: Props) => {
         </div>
         <div>
           visibility
-          <input {...visibility} />
+          <select value={visibility} onChange={onVisibilityChange}>
+            {visibilityOptions.map((v) => (
+              <option key={v.value} value={v.value}>
+                {v.label}
+              </option>
+            ))}
+          </select>
         </div>
         <div>
           weather
-          <input {...weather} />
+          <select value={weather} onChange={onWeatherChange}>
+            {weatherOptions.map((v) => (
+              <option key={v.value} value={v.value}>
+                {v.label}
+              </option>
+            ))}
+          </select>
         </div>
         <div>
           comment
